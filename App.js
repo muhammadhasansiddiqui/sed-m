164fdc6607921a0935d67a4b80b629c8a323576d
muhammadhasansiddiqui <hasansiddiqui464@gmail.com>
2024-02-26 18:55:06 +0500

    add bg

--- App.js
+++ App.js
@@ -13,8 +13,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'white',
-    alignItems: 'center',
+    backgroundColor: 'red',
+    alignItems: 'blue',
     justifyContent: 'center',
   },
 });
